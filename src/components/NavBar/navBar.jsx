4a18ed2a2a19c916d8450792d645deb81a799962
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-06-03 23:36:38 +0530

    ADD: added a navBar component

---
+++ src/components/NavBar/navBar.jsx
@@ -0,0 +1,12 @@
+import "../Navbar/navBar.css";
+
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h3> JustYle </h3>
+      <h3> Products </h3>
+      <h3> Wishlist </h3>
+      <h3> Cart </h3>
+    </div>
+  );
+};
