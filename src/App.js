1d78e2f5ab658f107b7b222f302c63af610bb15f
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-06-06 14:49:55 +0530

    update: updated the app.js and index.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Home } from "./pages/Home/home";
 import { ProductsListing } from "./pages/ProductsListing/productsListing";
+import { Cart } from "./pages/Cart/cart";
+import { Wishlist } from "./pages/Wishlist/wishlist";
+import { Signup } from "./pages/Signup/signup";
+import { Login } from "./pages/Login/login";
+import { ProductDetail } from "./pages/ProductDetail/productDetail";
 
 
 function App() {
@@ -10,6 +15,13 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<ProductsListing /> } />
+        <Route path="/cart" element={<Cart /> } />
+        <Route path="/wishlist" element={<Wishlist /> } />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/product/:productId" element={<ProductDetail />} />
+
+        
       </Routes>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
 import { DataContextProvider } from "./contexts/dataContext";
 import { FilterContextProvider } from "./contexts/filterContext";
+import { AuthContextProvider } from "./contexts/authContext";
+import { CartContextProvider } from "./contexts/cartContext";
+import { WishlistContextProvider } from "./contexts/wishlistContext";
 
 // Call make Server
 makeServer();
@@ -13,12 +16,17 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <DataContextProvider>
-        <FilterContextProvider>
-            <App />
-        </FilterContextProvider>
-       
-     </DataContextProvider>     
+      <AuthContextProvider>
+        <DataContextProvider>
+          <FilterContextProvider>
+            <CartContextProvider>
+              <WishlistContextProvider>
+              <App />
+              </WishlistContextProvider>
+            </CartContextProvider>   
+          </FilterContextProvider>
+        </DataContextProvider>   
+      </AuthContextProvider> 
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
