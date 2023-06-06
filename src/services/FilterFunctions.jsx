888929b2446bca5aef0e589b081d568cd2b2066a
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-06-06 14:47:57 +0530

    refactor: updated and added more filters

--- src/services/FilterFunctions.jsx
+++ src/services/FilterFunctions.jsx
@@ -6,5 +6,27 @@ export const FilterFunctions = (dataState, filterState) => {
       filterState.selectedCategory.includes(product.category)
     );
   }
+
+  if (filterState.selectedPriceSort.length > 0) {
+    if (filterState.selectedPriceSort === "HTL") {
+      filteredProducts = filteredProducts.sort((a, b) => b.price - a.price);
+    }
+    if (filterState.selectedPriceSort === "LTH") {
+      filteredProducts = filteredProducts.sort((a, b) => a.price - b.price);
+    }
+  }
+
+  if (filterState.selectedRating.length > 0) {
+    filteredProducts = filteredProducts.filter(
+      ({ rating }) => Number(rating) >= Number(filterState.selectedRating)
+    );
+  }
+
+  if (filterState.searchedValue.length > 0) {
+    filteredProducts = filteredProducts.filter(({ title }) =>
+      title.toLowerCase().includes(filterState.searchedValue.toLowerCase())
+    );
+  }
+
   return filteredProducts;
 };
