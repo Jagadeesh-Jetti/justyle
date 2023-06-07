43f0014273fb73e761eceb4a3392c7a305f7a267
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-06-07 13:33:22 +0530

    refactor: updated the login page code

--- src/pages/Login/login.jsx
+++ src/pages/Login/login.jsx
@@ -5,26 +5,50 @@ import { useContext } from "react";
 import { authContext } from "../../contexts/authContext";
 
 export const Login = () => {
-  const { guestLogin } = useContext(authContext);
+  const {
+    guestLogin,
+    userLogin,
+    userLoginDetails,
+    setUserLoginDetails,
+    logout,
+  } = useContext(authContext);
+
   return (
     <div>
       <Navbar />
       <form className="login">
         <div>
           <label htmlFor="email">Email:</label>
-          <input type="text" id="email" />
+          <input
+            type="text"
+            id="email"
+            onChange={(e) =>
+              setUserLoginDetails({
+                ...userLoginDetails,
+                email: e.target.value,
+              })
+            }
+          />
         </div>
         <div>
           <label htmlFor="password">Password:</label>
-          <input type="password" id="password" />
+          <input
+            type="password"
+            id="password"
+            onChange={(e) =>
+              setUserLoginDetails({
+                ...userLoginDetails,
+                password: e.target.value,
+              })
+            }
+          />
         </div>
-        <button>Login</button>
+        <button onClick={() => userLogin()}>Login</button>
       </form>
-      <button
-        onClick={() => guestLogin("adarshbalika@gmail.com", "adarshbalika")}
-      >
-        Guest Login
-      </button>
+
+      <button onClick={() => guestLogin()}> Guest Login </button>
+
+      <button onClick={() => logout()}> Log out </button>
       <h3>Don't have an account?</h3>
       <Link to="/signup">Sign up</Link>
     </div>
