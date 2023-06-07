b1d4479f4dc7c84590bff00d2e1d0669c727018b
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-06-07 13:29:51 +0530

    refactor: updated the footer code

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import "../Footer/footer.css";
+
+export const Footer = () => {
+  return (
+    <div className=".main-container-footer">
+      <div className="first-container">
+        <h2> Justyle </h2>
+        <p> yoh Jaggu! caption here! </p>
+        <Link>
+          <p> github </p>
+        </Link>
+        <Link>
+          <p> twitter </p>
+        </Link>
+        <Link>
+          <p> linkedin </p>
+        </Link>
+      </div>
+
+      <div className="second-container">
+        <h3> Quick Links</h3>
+        <Link to="/products">
+          <p> Products </p>
+        </Link>
+        <Link to="/cart">
+          <p> Cart </p>
+        </Link>
+        <Link to="/wishlist">
+          <p> Wishlist </p>
+        </Link>
+      </div>
+    </div>
+  );
+};
