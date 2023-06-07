cf6edd336e13e833778482568cfde184ac2fc7b5
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-06-07 13:33:12 +0530

    refactor: updated the home page code

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -7,6 +7,8 @@ import { filterContext } from "../../contexts/filterContext";
 import { FILTERACTIONS } from "../../reducers/Actions/FIlterActions";
 
 import "../Home/home.css";
+import { Footer } from "../../components/Footer/footer";
+import { Loader } from "../../components/Loader/loader";
 
 export const Home = () => {
   const { dataState } = useContext(dataContext);
@@ -16,41 +18,54 @@ export const Home = () => {
       <Navbar />
       <h2> Select from various categories</h2>
 
-      <div className="category_cards_container">
-        <div className="category_cards">
-          {dataState.categories &&
-            dataState?.categories?.map(({ _id, thumbnail, categoryName }) => {
-              return (
-                <Link
-                  className="category_img_link"
-                  to="/products"
-                  key={_id}
-                  onClick={() =>
-                    filterDispatch({
-                      type: FILTERACTIONS.FILTER_BY_CATEGORY,
-                      payload: categoryName,
-                    })
+      {dataState.categories.length === 0 ? (
+        <Loader />
+      ) : (
+        <>
+          <div className="category_cards_container">
+            <div className="category_cards">
+              {dataState.categories &&
+                dataState?.categories?.map(
+                  ({ _id, thumbnail, categoryName }) => {
+                    return (
+                      <Link
+                        className="category_img_link"
+                        to="/products"
+                        key={_id}
+                        onClick={() =>
+                          filterDispatch({
+                            type: FILTERACTIONS.FILTER_BY_CATEGORY,
+                            payload: categoryName,
+                          })
+                        }
+                      >
+                        <img
+                          src={thumbnail}
+                          alt={categoryName}
+                          className="category_image"
+                        />
+                      </Link>
+                    );
                   }
-                >
-                  <img
-                    src={thumbnail}
-                    alt={categoryName}
-                    className="category_image"
-                  />
-                </Link>
-              );
-            })}
-        </div>
-        <div className="category_name_container">
-          {dataState.categories &&
-            dataState?.categories?.map(({ _id, categoryName }) => {
-              return (
-                <p key={_id} className="category_text_name">
-                  {categoryName}
-                </p>
-              );
-            })}
-        </div>
+                )}
+            </div>
+
+            <div className="category_name_container">
+              {dataState.categories &&
+                dataState?.categories?.map(({ _id, categoryName }) => {
+                  return (
+                    <p key={_id} className="category_text_name">
+                      {categoryName}
+                    </p>
+                  );
+                })}
+            </div>
+          </div>
+        </>
+      )}
+
+      <div className="footer">
+        <Footer />
       </div>
     </div>
   );
