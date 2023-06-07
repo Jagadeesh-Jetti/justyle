9764ab5f2687b67af4eb0dd4674432fc04bb8902
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-06-07 13:32:16 +0530

    refactor: refactored the cart page

--- src/pages/Cart/cart.jsx
+++ src/pages/Cart/cart.jsx
@@ -4,18 +4,33 @@ import { Navbar } from "../../components/NavBar/navBar";
 
 import { CartCard } from "../../components/CartCard/cartCard";
 import { PriceDetailsCard } from "../../components/PriceDetailsCard/priceDetailsCard";
+import "../Cart/cart.css";
+import { Footer } from "../../components/Footer/footer";
+import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
 
 export const Cart = () => {
   const { dataState } = useContext(dataContext);
-
   return (
     <div>
       <Navbar />
-      <h1>this is the cart </h1>
-      <div className="main-container">
-        <div>{<CartCard products={dataState?.cart} />}</div>
+      {dataState?.cart?.length === 0 ? (
+        <div>
+          <h4> Oops! Your cart is empty </h4>
+          <Button>
+            <Link to="/products"> Explore the store</Link>
+          </Button>
+        </div>
+      ) : (
+        <div className="main-container">
+          <div>{<CartCard products={dataState?.cart} />}</div>
 
-        <div> {<PriceDetailsCard products={dataState?.cart} />} </div>
+          <div> {<PriceDetailsCard products={dataState?.cart} />} </div>
+        </div>
+      )}
+
+      <div>
+        <Footer />
       </div>
     </div>
   );
