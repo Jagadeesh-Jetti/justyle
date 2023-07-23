3e3f5eb807bb0db2c0cc88cd7ddcf6c657f9f5b1
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-07-23 19:19:38 +0530

    style: styled wishlist page

--- src/pages/Wishlist/wishlist.jsx
+++ src/pages/Wishlist/wishlist.jsx
@@ -2,33 +2,34 @@ import { useContext } from "react";
 import { dataContext } from "../../contexts/dataContext";
 import { Navbar } from "../../components/NavBar/navBar";
 import { ProductCard } from "../../components/productCard/productCard";
-import "../Wishlist/wishlist.css";
 import { Footer } from "../../components/Footer/footer";
 import { Link } from "react-router-dom";
 import { Button } from "@mui/material";
+import "./wishlist.css";
 
 export const Wishlist = () => {
   const { dataState } = useContext(dataContext);
 
   return (
-    <div>
+    <div className="wishlist-page">
       <Navbar />
-      {dataState?.wishlist?.length === 0 ? (
-        <div>
-          <h4> Ooops! Your Wishlist is empty</h4>
-          <Button>
-            <Link to="/products"> Explore the store</Link>
-          </Button>
-        </div>
-      ) : (
-        <div className="wishlist-container">
-          {<ProductCard fromWishlist={true} products={dataState?.wishlist} />}
-        </div>
-      )}
 
-      <div className="footer">
-        <Footer />
+      <div className="wishlist-content">
+        {dataState?.wishlist?.length === 0 ? (
+          <div className="wishlist-empty">
+            <h4>Ooops! Your Wishlist is empty</h4>
+            <Button>
+              <Link to="/products">Explore the store</Link>
+            </Button>
+          </div>
+        ) : (
+          <div className="wishlist-container">
+            <ProductCard fromWishlist={true} products={dataState?.wishlist} />
+          </div>
+        )}
       </div>
+
+      <Footer />
     </div>
   );
 };
