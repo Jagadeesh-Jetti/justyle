61b027415eca16adcb8946eaa5f1317e81be65e5
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-07-23 19:06:49 +0530

    style: styled cart page

--- src/pages/Cart/cart.jsx
+++ src/pages/Cart/cart.jsx
@@ -1,37 +1,42 @@
 import { useContext } from "react";
 import { dataContext } from "../../contexts/dataContext";
 import { Navbar } from "../../components/NavBar/navBar";
-
 import { CartCard } from "../../components/CartCard/cartCard";
 import { PriceDetailsCard } from "../../components/PriceDetailsCard/priceDetailsCard";
-import "../Cart/cart.css";
 import { Footer } from "../../components/Footer/footer";
 import { Link } from "react-router-dom";
 import { Button } from "@mui/material";
+import "./cart.css";
 
 export const Cart = () => {
   const { dataState } = useContext(dataContext);
+
   return (
-    <div>
+    <div className="cart-page">
       <Navbar />
-      {dataState?.cart?.length === 0 ? (
-        <div>
-          <h4> Oops! Your cart is empty </h4>
-          <Button>
-            <Link to="/products"> Explore the store</Link>
-          </Button>
-        </div>
-      ) : (
-        <div className="main-container">
-          <div>{<CartCard products={dataState?.cart} />}</div>
 
-          <div> {<PriceDetailsCard products={dataState?.cart} />} </div>
-        </div>
-      )}
+      <div className="cart-content">
+        {dataState?.cart?.length === 0 ? (
+          <div className="cart-empty">
+            <h4>Oops! Your cart is empty</h4>
+            <Button>
+              <Link to="/products">Explore the store</Link>
+            </Button>
+          </div>
+        ) : (
+          <div className="main-container">
+            <div className="cart-card-container">
+              <CartCard products={dataState?.cart} />
+            </div>
 
-      <div>
-        <Footer />
+            <div className="price-details-container">
+              <PriceDetailsCard products={dataState?.cart} />
+            </div>
+          </div>
+        )}
       </div>
+
+      <Footer />
     </div>
   );
 };
