115f9737f75fa29af1e3b91234a66eb5159d64ad
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-07-27 18:23:56 +0530

    refactor: update product detail

--- src/pages/ProductDetail/productDetail.jsx
+++ src/pages/ProductDetail/productDetail.jsx
@@ -33,57 +33,80 @@ export const ProductDetail = () => {
     getClickedProduct();
   }, [productId]);
 
-  const { _id, image, title, original_price, price, size, rating } =
-    clickedProduct;
+  const {
+    _id,
+    image,
+    title,
+    description,
+    original_price,
+    price,
+    size,
+    rating,
+    reviews,
+  } = clickedProduct;
 
   return (
-    <div className="main-layout">
+    <div>
       <Navbar />
 
-      {Object.keys(clickedProduct).length === 0 ? (
-        <Loader />
-      ) : (
-        <>
-          <div className="cards-layout">
-            <div className="image-container">
-              <img src={image} alt="loading" className="image" />
+      <div className="main-layout">
+        {Object.keys(clickedProduct).length === 0 ? (
+          <Loader />
+        ) : (
+          <>
+            <div className="cards-layout">
+              <div className="image-container">
+                <img src={image} alt="loading" className="image" />
+              </div>
+
+              <div>
+                <h2> {title} </h2>
+                <div>{description}</div>
+
+                <div className="reviews_class">
+                  <div> {rating} </div>
+                  <div className="hr"> | </div>
+                  <div>{reviews}</div>
+                </div>
+                <div>
+                  <div className="price">
+                    <div> ₹{price} </div>
+                    <div className="original-price"> ₹{original_price} </div>
+                  </div>
+                  <div> inclusive of all Taxes</div>
+                </div>
+
+                <p> Size: {size} </p>
+
+                <button
+                  onClick={() => {
+                    isProductInWishlist(_id)
+                      ? navigate("/cart")
+                      : addToWishlistHandler(clickedProduct, dataDispatch);
+                  }}
+                >
+                  {isProductInWishlist(_id)
+                    ? "Go to Wishlist"
+                    : "Add to Wishlist"}
+                </button>
+
+                <button
+                  onClick={() => {
+                    isProductInCart(_id)
+                      ? navigate("/cart")
+                      : addToCartHandler(clickedProduct, dataDispatch);
+                  }}
+                >
+                  {isProductInCart(_id) ? "Go to Cart" : "Add to Cart"}
+                </button>
+              </div>
             </div>
-
-            <div>
-              <h3> {title} </h3>
-              <p> {original_price} </p>
-              <p> {price} </p>
-              <p> {size} </p>
-              <p> {rating} </p>
-
-              <button
-                onClick={() => {
-                  isProductInWishlist(_id)
-                    ? navigate("/cart")
-                    : addToWishlistHandler(clickedProduct, dataDispatch);
-                }}
-              >
-                {isProductInWishlist(_id)
-                  ? "Go to Wishlist"
-                  : "Add to Wishlist"}
-              </button>
-
-              <button
-                onClick={() => {
-                  isProductInCart(_id)
-                    ? navigate("/cart")
-                    : addToCartHandler(clickedProduct, dataDispatch);
-                }}
-              >
-                {isProductInCart(_id) ? "Go to Cart" : "Add to Cart"}
-              </button>
+            <div className="footer">
+              <Footer />
             </div>
-          </div>
-          <div className="footer">
-            <Footer />
-          </div>
-        </>
-      )}
+          </>
+        )}
+      </div>
     </div>
   );
 };
