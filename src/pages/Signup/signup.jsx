6dd87be16704a0894ecc90e7c3dfdfe5628b4f1b
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-08-04 14:42:34 +0530

    refactor: updated the signup page code

--- src/pages/Signup/signup.jsx
+++ src/pages/Signup/signup.jsx
@@ -1,12 +1,39 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import "../Signup/signup.css";
 import { Navbar } from "../../components/NavBar/navBar";
 import { authContext } from "../../contexts/authContext";
 
 export const Signup = () => {
-  const { userDetails, setUserDetails, signupHandler } =
-    useContext(authContext);
+  const { signupHandler } = useContext(authContext);
+
+  const [userDetails, setUserDetails] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    if (
+      userDetails.firstName.trim() === "" ||
+      userDetails.lastName.trim() === "" ||
+      userDetails.email.trim() === "" ||
+      userDetails.password.trim() === "" ||
+      userDetails.confirmPassword.trim() === ""
+    ) {
+      console.log("Fields are empty");
+      return;
+    }
+
+    if (userDetails.password !== userDetails.confirmPassword) {
+      console.log("Passwords do not match");
+      return;
+    }
+    signupHandler(userDetails);
+  };
 
   return (
     <div>
@@ -85,7 +112,7 @@ export const Signup = () => {
                 }
               />
             </div>
-            <button onClick={signupHandler}> Create account </button>
+            <button onClick={handleSignup}>Create account</button>
             <div className="signup-footer">
               <h3> Have an account already? </h3>
               <Link to="/login">Login</Link>
