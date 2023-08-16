b6d1393339a624b3408fc1410d61baee67dc8156
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-08-16 15:19:29 +0530

    refactor: updated orderConfirmation page code

--- src/pages/OrderConfirmation/orderConfirmation.jsx
+++ src/pages/OrderConfirmation/orderConfirmation.jsx
@@ -1,13 +1,24 @@
 import { useNavigate } from "react-router-dom";
 import { Navbar } from "../../components/NavBar/navBar";
+import { useContext } from "react";
+import { dataContext } from "../../contexts/dataContext";
 
 export const OrderConfirmation = () => {
   const navigate = useNavigate();
+  const { dataState } = useContext(dataContext);
 
+  console.log(dataState.orderConfirmation);
   return (
     <div>
       <Navbar />
-      <h3> Order placed sucessfully </h3>
+      <h3> Order placed successfully </h3>
+      <div>
+        {dataState.orderConfirmation.map((orderItem) => (
+          <div key={orderItem._id} className="order-item">
+            <div>{orderItem.title}</div>
+          </div>
+        ))}
+      </div>
       <button onClick={() => navigate("/products")}> Go to products </button>
     </div>
   );
