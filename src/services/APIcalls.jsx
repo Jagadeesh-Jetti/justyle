f0cb9a3e368931bcb05ad4150bcb7b0f40d9f95f
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-08-16 15:24:12 +0530

    refactor: added a API call

--- src/services/APIcalls.jsx
+++ src/services/APIcalls.jsx
@@ -3,3 +3,22 @@ import axios from "axios";
 export const getProducts = async () => axios.get("/api/products");
 
 export const getCategories = async () => axios.get("/api/categories");
+
+export const removeFromCartHandler = async (dispatchData, itemId) => {
+  try {
+    const encodedToken = localStorage.getItem("userToken");
+    const response = await axios.delete(`/api/user/cart/${itemId}`, {
+      headers: {
+        authorization: encodedToken,
+      },
+    });
+    if (response?.status === 200) {
+      dispatchData({
+        type: "SET_CART_ITEMS",
+        payload: response?.data?.cart,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
